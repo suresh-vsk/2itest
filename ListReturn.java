28f3170b789a694202b52f84f94a5f45a1f8b3a2
suresh-vsk <31981321+suresh-vsk@users.noreply.github.com>
2023-05-13 11:26:10 +0100

    Add Code

--- ListReturn.java
+++ ListReturn.java
@@ -1,30 +1,38 @@
 package org.example;
 
 import java.util.ArrayList;
- import java.util.List;
-import java.util.Random;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class ListReturn {
 
-public static List<Integer> getNonDuplicateSortedList(int min, int max) {
-
-    List<Integer> numbers = new ArrayList<>();
-    for(int i = 0; i < 10; i++) {
-        numbers.add(new Random().ints(min, (max + 1)).findFirst().getAsInt());
+    public static List<Integer> getNonDuplicateSortedList(List<Integer> list, int min, int max) {
+        if (list.stream().allMatch(i -> i >= min && i <= max)) {
+            return list.stream()
+                    .distinct()
+                    .sorted()
+                    .collect(Collectors.toList());
+        } else {
+            throw new IllegalArgumentException("Input list " + list + " is not with in the range" + min + "and" + max);
+        }
     }
 
-    return numbers.stream()
-            .distinct()
-            .sorted()
-            .collect(Collectors.toList());
-}
-
-public static void main(String[] args) {
-    List<Integer> subList = getNonDuplicateSortedList(1, 100);
-    System.out.println("Sub list is");
-    for (Integer a : subList) {
-        System.out.print(a + " ");
+    public static void main(String[] args) {
+        List<Integer> intList = new ArrayList<Integer>();
+        intList.add(100);
+        intList.add(24);
+        intList.add(45);
+        intList.add(99);
+        intList.add(44);
+        intList.add(77);
+        intList.add(40);
+        intList.add(87);
+        intList.add(33);
+        intList.add(62);
+        List<Integer> newList = getNonDuplicateSortedList(intList, 1, 100);
+        System.out.println("Sub list is");
+        for (Integer a : newList) {
+            System.out.print(a + " ");
+        }
     }
-  }
 }
